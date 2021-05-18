b21c2536630a3a3736d796a2640ae11198493987
Veronika Ozretic <vozret00@fesb.hr>
2021-05-18 15:29:06 +0200

    dealing with submission and input values (198)

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,38 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    console.log(enteredName);
+    const enteredValue = nameInputRef.current.value;
+    console.log("Using useRef: " + enteredValue);
+
+    setEnteredName("");
+
+    // with refs, clearing out the input is not so elegant
+    // this is not ideal because we are directly manipulating the DOM
+    // nameInputRef.current.value = "";
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
